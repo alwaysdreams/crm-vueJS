a793cef008000b54fdf48e4c8321f89c02bb98af
Roman Zastavnyi <roma.zastavnyi@gmail.com>
2019-07-03 14:30:18 +0300

    modify navigation and side bar

---
+++ src/components/app/Sidebar.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul class="sidenav app-sidenav" :class="{open: value}">
+
+    <router-link
+      v-for="item in links"
+      :key="item.url"
+      active-class="active"
+      tag="li"
+      :to="item.url"
+      :exact="item.exact"
+    >
+        <a href="#" class="waves-effect waves-orange pointer">{{ item.title }}</a>
+    </router-link>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['value'],
+  data: () => ({
+    links: [
+      { title: 'Счет', url: '/detail', id: 1, exact: true },
+      { title: 'История', url: '/history', id: 2 },
+      { title: 'Планирование', url: '/planning', id: 3 },
+      { title: 'Новая запись', url: '/record', id: 4 },
+      { title: 'Категории', url: '/categories', id: 5 }
+    ]
+  })
+}
+</script>
